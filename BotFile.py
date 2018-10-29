b4649df7cdedc545ff15f2c53f2f5b367779d84d
Stranger94 <35736068+Stranger94@users.noreply.github.com>
2018-10-29 19:43:51 +0100

    Update BotFile.py

--- BotFile.py
+++ BotFile.py
@@ -69,7 +69,7 @@ async def tempmute(ctx, user: discord.Member):
             embed.set_thumbnail(url=user.avatar_url)
             await bot.add_roles(user, role)
             await bot.say(embed=embed)
-            rolex = discord.utils.get(user.server.roles, name='GiveawayWinner') 
+            rolex = discord.utils.get(user.server.roles, "504819720197898252" in (role.id for role in ctx.message.author.roles)) 
             await bot.remove_roles(ctx.message.author, rolex)
 
             await asyncio.sleep(300)

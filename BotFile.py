4c0bc55d34802ff039478e98c240b932afc8c962
Stranger94 <35736068+Stranger94@users.noreply.github.com>
2018-10-29 22:06:29 +0100

    Update BotFile.py

--- BotFile.py
+++ BotFile.py
@@ -90,11 +90,12 @@ async def tempmute(ctx, user: discord.Member):
             await bot.say(embed=embed)
             
 @bot.command(pass_context=True)
-async def guess(ctx, y: int):
+async def guess(ctx, y: str):
     if ("504819720197898252" in (role.id for role in ctx.message.author.roles)):
         embed = discord.Embed(title="You cannot win something, you already have!", description="You have AngryMod role already!", color=0xff0000)
         await bot.say(embed=embed)
     else:
+            y = int(y)
             a = random.randint(1, 100)
             await bot.say("Ok, lemme roll the dice")
             await asyncio.sleep(2)

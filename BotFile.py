c1ed29d7345a69f6f6ef7815624691761d4e7a7d
Stranger94 <35736068+Stranger94@users.noreply.github.com>
2018-10-29 21:48:25 +0100

    Update BotFile.py

--- BotFile.py
+++ BotFile.py
@@ -88,7 +88,31 @@ async def tempmute(ctx, user: discord.Member):
     else:
             embed = discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff0000)
             await bot.say(embed=embed)
+            
+@bot.command(pass_context=True)
+async def guess(ctx, y):
+    if ("504819720197898252" in (role.id for role in ctx.message.author.roles)):
+        embed = discord.Embed(title="You cannot win something, you already have!", description="You have AngryMod role already!", color=0xff0000)
+        await bot.say(embed=embed)
+    else:
+            a = random.randint(1, 100)
+            await bot.say("Ok, lemme roll the dice")
+            await asyncio.sleep(2)
+            await bot.say("...")
+            await asyncio.sleep(3)
+            await bot.say("...")
+            await asyncio.sleep(5)
+            await bot.say("I rolled a " + str(a) + "!") 
+            
 
+            if y == a:
+                 await bot.add_roles(ctx.message.author, rolex)
+                 embed = discord.Embed(title="Congratulations {}! You win the AngryMod role.".format(user.name) , color=0x0072ff)
+                 await bot.say(embed=embed) 
+            else:
+                 embed = discord.Embed(title="{} guessed wrong HAHA".format(user.name) , color=0x0f72ff)
+                 await bot.say(embed=embed) 
+                 
 
 bot.run(os.getenv('TOKEN'))
 

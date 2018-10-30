7f28640d8d7db1ca66e33b5f4ca2ba7ab086a42e
Stranger94 <35736068+Stranger94@users.noreply.github.com>
2018-10-30 18:46:36 +0100

    Update BotFile.py

--- BotFile.py
+++ BotFile.py
@@ -48,6 +48,54 @@ async def on_ready():
 async def on_ready():
     client.loop.create_task(status())
 
+os.chdir(r'C:\Users\Richard\Desktop\Discord Bot Domination')
+
+#config = default.get("users.json")
+
+@bot.event
+async def on_member_join(member):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users,member)
+
+    with open('users.json', 'w') as f:
+         json.dump(users, f)
+
+@bot.event
+async def on_message(message):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users,message.author)
+    await add_experience(users,message.author, 5)
+    await level_up(users,message.author, message.channel)
+
+
+    with open('users.json', 'w') as f:
+         json.dump(users, f)
+
+async def update_data(users,user):
+    if not user.id in users:
+        users[user.id] = {}
+        users[user.id]['experience'] = 0
+        users[user.id]['level'] = 1
+
+async def add_experience(users,user,exp):
+    users[user.id]['experience']    += exp
+
+async def level_up(users,user,channel):
+    experience = users[user.id]['experience']
+    level_start = users[user.id]['level']
+    level_end = int(experience ** (1/4))
+
+    if level_start < level_end:
+        await bot.send_message(channel, '{} has leveled up to level {}'.format(user.mention, level_end))
+        users[user.id]['level'] = level_end   
+    
+    
+    
+    
 @commands.cooldown(1, 10, commands.BucketType.user)
 
 

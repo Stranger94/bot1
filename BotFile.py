13273b62a6011405818c8f637c6aec4b6c02aae2
Stranger94 <35736068+Stranger94@users.noreply.github.com>
2018-10-31 01:19:39 +0100

    Update BotFile.py

--- BotFile.py
+++ BotFile.py
@@ -56,7 +56,7 @@ async def on_ready():
 
 @bot.command(pass_context = True)
 async def ping(ctx):
-       embed = discord.Embed(title= "Bot by Stranger#1405", description="Last update: 30.10.2018 02:38" , color=0x80f43d)
+       embed = discord.Embed(title= "Bot by Stranger#1405", description="Last update: 31.10.2018 01:20" , color=0x80f43d)
        await bot.say(embed=embed)
       
 @commands.cooldown(1, 30, commands.BucketType.user)
@@ -103,34 +103,61 @@ async def tempmute(ctx, user: discord.Member):
         else:
             embed = discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff0000)
             await bot.say(embed=embed)
-@commands.cooldown(3, 120, commands.BucketType.user)
-            
+@commands.cooldown(2, 120, commands.BucketType.user)            
 @bot.command(pass_context=True)
 async def guess(ctx, y: int):
     if ("504819720197898252" in (role.id for role in ctx.message.author.roles)):
-        embed = discord.Embed(title="You cannot win something, you already have!", description="You have AngryMod role already!", color=0xff0000)
+        embed = discord.Embed(title="You cannot win something, you already have!", description="You have AngryMod role already!", color=0xfcf7f8)
         await bot.say(embed=embed)
     else:
+
+        if (y > 30 or y < 1):
+            embed = discord.Embed(title="Unknown constructions? Dick stuck in sink.", description="Guess from 1 to 30, and nothing weird!", color=0x660e1b)
+            await bot.say(embed=embed)
+        else:
             a = random.randint(1, 30)
             await bot.say("Ok, lemme roll the dice")
             await asyncio.sleep(3)
             await bot.say("...")
             await asyncio.sleep(3)
             await bot.say("...")
-            await asyncio.sleep(3)
+            await asyncio.sleep(1)
             await bot.say("I rolled a " + str(a) + "!") 
             
-
             if y == a:
                  
                  rolex = discord.utils.get(ctx.message.server.roles, name='AngryMod')
                  await bot.add_roles(ctx.message.author, rolex)
-                 embed = discord.Embed(title="Congratulations {}! You win the AngryMod role.".format(ctx.message.author.name) , color=0xff72ff)
+                 embed = discord.Embed(title="Congratulations {}! You win the AngryMod role.".format(ctx.message.author.name) , color=0x7cff30)
                  await bot.say(embed=embed)
-                  
             else:
-                 embed = discord.Embed(title="{} guessed wrong HAHA".format(ctx.message.author.name) , color=0xf072ff)
-                 await bot.say(embed=embed) 
+
+                if ((y + 2) == a or (y + 1) == a or (y - 1) == a or (y - 2) == a):
+                 
+                        embed = discord.Embed(title= "Very scarce oO", description="{} almost guessed right. Dammit.".format(ctx.message.author.name) , color=0xc99206)
+                        await bot.say(embed=embed)
+                        await bot.say("Ok...Rerolling.")
+                        a = random.randint(1, 30)
+                        await asyncio.sleep(3)
+                        await bot.say("...")
+                        await asyncio.sleep(3)
+                        await bot.say("...")
+                        await asyncio.sleep(3)
+                        await bot.say("I rolled a " + str(a) + "!") 
+                
+                        if y == a:
+                 
+                            await bot.add_roles(ctx.message.author, rolex)
+                            embed = discord.Embed(title="Congratulations {}! You win the AngryMod role.".format(ctx.message.author.name) , color=0x7cff30)
+                            await bot.say(embed=embed)
+                  
+                        else:
+                            embed = discord.Embed(title="{} guessed wrong and I won't reroll!".format(ctx.message.author.name) , color=0xdb0020)
+                            await bot.say(embed=embed) 
+                else:
+                        embed = discord.Embed(title="{} can't defeat me!".format(ctx.message.author.name) , color=0xdb0020)
+                        await bot.say(embed=embed) 
+                 
                  
 @commands.cooldown(2, 300, commands.BucketType.user)
 
